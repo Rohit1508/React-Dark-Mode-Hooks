7dd1854a0b5db96dc17c816efc9996e8c9b9f307
rohit13.singh <rohiit.kumar.singh@gmail.com>
2020-04-05 02:22:18 +0530

    Dark mode changes added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DarkModeToggle from './DarkMode'
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <DarkModeToggle />
         </a>
       </header>
     </div>
